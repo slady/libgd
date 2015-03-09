3a2813bbcf9932d7ed72f335e3748152a58f25b5
Petr Sládek <petr.sladek@i.cz>
2015-03-09 14:53:42 +0100

    two GdImage constructors combined

---
+++ src/main/java/info/miranda/gd/GdImageColorType.java
@@ -0,0 +1,14 @@
+package info.miranda.gd;
+
+public enum GdImageColorType {
+	/**
+	 * Palette-based images, with no more than 256 colors.
+	 */
+	PALETTE_BASED_COLOR,
+	/**
+	 * Truecolor images, with an essentially unlimited number of colors.
+	 * Truecolor images are always filled with black at creation
+	 * time. There is no concept of a "background" color index.
+	 */
+	TRUE_COLOR,
+}
