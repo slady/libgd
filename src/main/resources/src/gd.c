f2d3ad6dd76c57739e904059212af3a1f9e5e6d9
Petr Sládek <petr.sladek@i.cz>
2015-03-11 10:03:32 +0100

    removed HWB_to_RGB

--- src/main/resources/src/gd.c
+++ src/main/resources/src/gd.c
@@ -110,52 +110,6 @@ HWB_Diff (int r1, int g1, int b1, int r2, int g2, int b2)
 }
 
 
-#if 0
-/*
- * This is not actually used, but is here for completeness, in case someone wants to
- * use the HWB stuff for anything else...
- */
-static RGBType *
-HWB_to_RGB (HWBType HWB, RGBType * RGB)
-{
-
-	/*
-	 * H is given on [0, 6] or UNDEFINED. W and B are given on [0, 1].
-	 * RGB are each returned on [0, 1].
-	 */
-
-	float h = HWB.H, w = HWB.W, b = HWB.B, v, n, f;
-	int i;
-
-	v = 1 - b;
-	if (h == HWB_UNDEFINED)
-		RETURN_RGB (v, v, v);
-	i = floor (h);
-	f = h - i;
-	if (i & 1)
-		f = 1 - f;			/* if i is odd */
-	n = w + f * (v - w);		/* linear interpolation between w and v */
-	switch (i) {
-	case 6:
-	case 0:
-		RETURN_RGB (v, n, w);
-	case 1:
-		RETURN_RGB (n, v, w);
-	case 2:
-		RETURN_RGB (w, v, n);
-	case 3:
-		RETURN_RGB (w, n, v);
-	case 4:
-		RETURN_RGB (n, w, v);
-	case 5:
-		RETURN_RGB (v, w, n);
-	}
-
-	return RGB;
-
-}
-#endif
-
 BGD_DECLARE(int) gdImageColorClosestHWB (gdImagePtr im, int r, int g, int b)
 {
 	int i;
